e775a1698daa9841bed127f7896f5555da644ce7
LandonPanos <28903662+Lpanos@users.noreply.github.com>
2024-01-19 12:57:41 -0800

    change firstOne and secondOne to their own values previously the values of the numbers were tied to the display.textContent which did not allow for flexibility. Changing these to their own values which update on every press has produced better results.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
 let firstNum = 0;
 let secondNum = 0;
-let operator = '';
+let operator = 0;
+let newOperator = 0;
+let calculating = 0;
+const operatorArray = ['+','-','/','x']
+let total = 0;
+
+const clearCalc = function(){
+    firstNum = 0;
+    secondNum = 0;
+    operator = 0;
+};
 
 const add = function(a, b) {
     return a + b;
@@ -17,27 +27,84 @@ const multiply = function(a, b) {
 const divide = function (a,b){
 return a / b;
 };
-  
+
 const power = function(a,b) {
     return a ** b;
 };
   
 const operate = function(firstNum,secondNum,operator){
+let total = 0;
     switch (operator){
-        case add:
-            add(firstNum,secondNum);
+        case '+':
+            total = add(firstNum,secondNum);
             break;
-        case subtract:
-            subtract(firstNum,secondNum);
+        case '-':
+            total = subtract(firstNum,secondNum);
             break;
-        case multiply:
-            multiply(firstNum,secondNum);
+        case 'x':
+            total = multiply(firstNum,secondNum);
             break;
-        case divide:
-            divide(firstNum,secondNum);
+        case '/':
+            total = divide(firstNum,secondNum);
             break;
-        case power:
-            power(firstNum,secondNum);
+        case '*':
+            total = power(firstNum,secondNum);
             break;
     }
+    return total;
 }
+
+const btn = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+
+btn.forEach(function(button){
+    button.addEventListener("click",function(){
+        if(button.textContent == 'C'){
+
+            clearCalc();
+
+            return display.textContent = '';
+        }
+
+
+        if (operatorArray.includes(button.textContent) && operator){
+
+            display.textContent = (operate(Number(firstNum), Number(secondNum),operator));
+                
+             clearCalc();
+
+            firstNum = display.textContent;
+
+            operator = button.textContent;
+
+            total = firstNum;
+
+        } else if(operatorArray.includes(button.textContent) && !(operator)){
+                    operator = button.textContent;
+                                                        console.log(operator + ' is operator LINE 90')
+                        display.textContent = '';
+
+            } else if(button.textContent == '=' && operator){
+                secondNum = display.textContent;
+                    console.log(secondNum + ' is secondNum LINE 95')
+                        display.textContent = operate(Number(firstNum), Number(secondNum),operator);
+                                clearCalc();
+                                    total = display.textContent;
+            } else { //if THE BUTTONS PRESSED ARE 0-9 THEN DO THIS SHICK
+                if(!(operator)){
+                    firstNum += button.textContent;
+                } else if(operator){
+                    secondNum += button.textContent;
+                        if(total){
+                            display.textContent = '';
+                                total = 0;
+                        }
+                }
+                    //if first number and operator already exist, when attempting to enter number 2 wipe the display.
+                    //(but not on every button press, we only want it to wipe on the first before pressing another operator)
+                display.textContent += button.textContent;
+            };
+
+
+    })
+})
